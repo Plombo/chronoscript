30a81ad3d0cc931ccaa5915ee1b8a5a54edbb6fc
Bryan Cain <bryancain3@gmail.com>
2019-02-07 17:01:50 -0700

    ScriptUtils: replace searchList with implementation from current OpenBOR

--- ScriptUtils.c
+++ ScriptUtils.c
@@ -50,104 +50,26 @@ void printEscapedString(const char *string)
 // Optimized search in an arranged string table, return the index
 int searchList(const char *list[], const char *value, int length)
 {
-    int i;
-    int a = 0;
-    int b = length / 2;
-    int c = length - 1;
-    int v = value[0];
+    int low = 0, high = length - 1, mid = 0;
 
-    // We must convert uppercase values to lowercase,
-    // since this is how every command is written in
-    // our source.  Refer to an ASCII Chart
-    if (v >= 0x41 && v <= 0x5A)
+    while (low <= high)
     {
-        v += 0x20;
-    }
-
-    // Index value equals middle value,
-    // Lets search starting from center.
-    if (v == list[b][0])
-    {
-        if (stricmp(list[b], value) == 0)
+        mid = (low + high) / 2;
+        if (stricmp(list[mid], value) < 0)
         {
-            return b;
+            low = mid + 1;
+        }
+        else if (stricmp(list[mid], value) == 0)
+        {
+            return mid;
+        }
+        else if (stricmp(list[mid], value) > 0)
+        {
+            high = mid - 1;
         }
 
-        // Search Down the List.
-        if (v == list[b - 1][0])
-        {
-            for (i = b - 1 ; i >= 0; i--)
-            {
-                if (stricmp(list[i], value) == 0)
-                {
-                    return i;
-                }
-                if (v != list[i - 1][0])
-                {
-                    break;
-                }
-            }
-        }
-
-        // Search Up the List.
-        if (v == list[b + 1][0])
-        {
-            for (i = b + 1; i < length; i++)
-            {
-                if (stricmp(list[i], value) == 0)
-                {
-                    return i;
-                }
-                if (v != list[i + 1][0])
-                {
-                    break;
-                }
-            }
-        }
-
-        // No match, return failure.
-        goto searchListFailed;
     }
 
-    // Define the starting point.
-    if (v >= list[b + 1][0])
-    {
-        a = b + 1;
-    }
-    else if (v <= list[b - 1][0])
-    {
-        c = b - 1;
-    }
-    else
-    {
-        goto searchListFailed;
-    }
-
-    // Search Up from starting point.
-    for (i = a; i <= c; i++)
-    {
-        if (v == list[i][0])
-        {
-            if (stricmp(list[i], value) == 0)
-            {
-                return i;
-            }
-            if (v != list[i + 1][0])
-            {
-                break;
-            }
-        }
-    }
-
-searchListFailed:
-
-    // The search failed!
-    // On five reasons for failure!
-    // 1. Is the list in alphabetical order?
-    // 2. Is the first letter lowercase in list?
-    // 3. Does the value exist in the list?
-    // 4. Is it a typo?
-    // 5. Is it a text file error?
     return -1;
 }
 
