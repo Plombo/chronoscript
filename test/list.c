b840346f05a8e78bcb6f37e78ced411732027e81
Bryan Cain <bryancain3@gmail.com>
2019-03-14 10:44:17 -0600

    Update test/list.c to use methods instead of builtins.
    The builtin versions of those functions might be removed from
    ChronoScript in the future. There's no longer any need for them.

--- test/list.c
+++ test/list.c
@@ -17,18 +17,26 @@ void main()
     expect(list[2], "c");
 
     // append an element to the list
-    list_append(list, "e");
+    list.append("e");
     log(list);
     expect(list[3], "e");
 
     // insert an element in the middle of the list
-    list_insert(list, 3, "f");
+    list.insert(3, "f");
     log(list);
     expect(list[3], "f");
     expect(list[4], "e");
 
-    list_remove(list, 3);
+    list.remove(3);
     log(list);
     expect(list[3], "e");
+
+    list.remove(0);
+    log(list);
+    list.remove(0);
+    log(list);
+    list.remove(0);
+    list.remove(0);
+    expect(list + "", "[]");
 }
 
